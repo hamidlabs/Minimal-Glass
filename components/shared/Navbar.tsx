6c16d65d097b4a8a97edaf6e5f7aae2d33cd501c
somiya <somiyaakterrimo2021@gmail.com>
2025-09-13 20:36:00 +0600

    fixed nabvar logo M

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -58,7 +58,7 @@ export default function Navbar() {
                 isMenuOpen ? "text-black" : ""
               )}
             >
-              A
+              M
             </div>
 
             {/* Right Icons */}
